338e2d943e7eb66ce20751309a7772900f884d2a
Jared <jbates9210@gmail.com>
2018-05-11 11:04:02 -0400

    postcss tools and autoprefixer added

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
+
 /*
 gulp.task('default', function() {
   console.log('Gulp task created.');
@@ -12,7 +17,9 @@ gulp.task('html', function() {
 */
 
 gulp.task('styles', function() {
-  return gulp.src('./docs/assets/styles/styles.css').pipe(gulp.dest('./docs/temp/styles'));
+  return gulp.src('./docs/assets/styles/styles.css')
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./docs/temp/styles'));
 });
 
 gulp.task('watch', function() {
